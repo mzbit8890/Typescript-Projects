8e42af6d389793476f2352766e3d572b01c21925
Sana Zahid <139183581+mzbit8890@users.noreply.github.com>
2023-11-05 14:46:58 +0500

    Add files via upload
    "Welcome to the TypeScript ATM Project - Your Secure Gateway to Financial Freedom! Our state-of-the-art ATM system, powered by TypeScript, offers a seamless and secure banking experience. Whether you're checking balances, making withdrawals, or managing your finances, our ATM project ensures your transactions are accurate, confidential, and user-friendly. Empower your financial journey with the confidence that only TypeScript can provide. Discover the future of banking today!"

--- main.js
+++ main.js
@@ -1,37 +1,84 @@
 import inquirer from "inquirer";
-async function main() {
-    let { firstNumber, secondNumber, operation } = await inquirer.prompt([
-        {
-            message: "Enter first Number",
+import { faker } from "@faker-js/faker";
+import chalk from "chalk";
+const createUser = () => {
+    let users = [];
+    for (let i = 0; i <= 10; i++) { // Adjusted loop to run 11 times
+        let userObj = {
+            id: i,
+            pin: 1000 + i,
+            name: faker.person.fullName(),
+            accountNumber: Math.floor(100000000 * Math.random() * 90000000),
+            balance: 1000000 * i,
+        };
+        users.push(userObj);
+    }
+    return users;
+};
+// ATM machine
+const atmMachine = async (users) => {
+    try {
+        const response = await inquirer.prompt({
             type: "number",
-            name: "firstNumber"
-        },
-        {
-            message: "Enter second Number",
-            type: "number",
-            name: "secondNumber"
-        },
-        {
-            name: "operation",
-            type: "list",
-            choices: ["addition", "subtraction", "multiplication", "division"],
-            message: "Enter your operation"
+            message: "Enter your PIN code",
+            name: "pin",
+        });
+        const findingUser = users.find((user) => user.pin === response.pin);
+        if (findingUser) {
+            console.log(chalk.green(`Welcome ${findingUser.name} to your Account`));
+            await atmFunc(findingUser);
         }
-    ]);
-    if (operation === "addition") {
-        console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+        else {
+            console.log(chalk.red("Invalid User Pin"));
+        }
     }
-    else if (operation === "subtraction") {
-        console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+    catch (error) {
+        console.error(chalk.red("An error occurred: " + error));
     }
-    else if (operation === "multiplication") {
-        console.log(`${firstNumber} x ${secondNumber} = ${firstNumber * secondNumber}`);
+};
+const atmFunc = async (findingUser) => {
+    try {
+        const answer = await inquirer.prompt({
+            type: "list",
+            name: "Select",
+            message: "Choose the selection that you want to do",
+            choices: ["Withdraw", "Balance", "Deposit", "Exit"],
+        });
+        if (answer.Select === "Withdraw") {
+            const amount = await inquirer.prompt({
+                type: "number",
+                message: "Enter Amount",
+                name: "amount",
+            });
+            if (amount.amount > findingUser.balance) {
+                console.log(chalk.red("Insufficient Balance"));
+            }
+            else {
+                findingUser.balance -= amount.amount;
+                console.log(chalk.green(`Withdraw amount: ${amount.amount}`));
+                console.log(chalk.yellow(`Total Balance: ${findingUser.balance}`));
+            }
+        }
+        if (answer.Select === "Balance") {
+            console.log(chalk.yellow(`Balance: ${findingUser.balance}`));
+        }
+        if (answer.Select === "Deposit") {
+            const deposit = await inquirer.prompt({
+                type: "number",
+                message: "Enter Deposit Amount",
+                name: "amount",
+            });
+            findingUser.balance += deposit.amount;
+            console.log(chalk.green(`Deposit amount: ${deposit.amount}`));
+            console.log(chalk.yellow(`Total Balance: ${findingUser.balance}`));
+        }
+        if (answer.Select === "Exit") {
+            console.log(chalk.blue("Thanks For Using This ATM...."));
+        }
     }
-    else if (operation === "division") {
-        console.log(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+    catch (error) {
+        console.error(chalk.red("An error occurred: " + error));
     }
-    else {
-        console.log("Invalid operation");
-    }
-}
-main();
+};
+const users = createUser();
+atmMachine(users);
--- main.ts
+++ main.ts
@@ -1,38 +1,105 @@
 import inquirer from "inquirer";
+import { faker } from "@faker-js/faker";
+import chalk from "chalk";
 
-async function main () {
-    let {firstNumber, secondNumber, operation} = await inquirer.prompt([
-{
-    message : "Enter first Number",
-    type:"number",
-    name:"firstNumber"
-},
-{
-    message: "Enter second Number",
-    type: "number",
-    name: "secondNumber"
-},
-{
-    name: "operation",
-    type: "list",
-    choices: ["addition", "subtraction", "multiplication", "division"],
-    message: "Enter your operation"
+// Requirements:
+// 1. User data = complete
+// 2. ATM machine
+// 3. ATM functions
+
+interface User {
+  name: string;
+  id: number;
+  pin: number;
+  accountNumber: number;
+  balance: number;
 }
-    ])
 
-    if(operation === "addition"){
-        console.log(`${firstNumber} + ${secondNumber} = ${firstNumber+secondNumber}`)
-    } else if(operation === "subtraction"){
-        console.log(`${firstNumber} - ${secondNumber} = ${firstNumber-secondNumber}`)
-    } else if(operation === "multiplication"){
-        console.log(`${firstNumber} x ${secondNumber} = ${firstNumber*secondNumber}`)
-    }else if(operation === "division"){
-        console.log(`${firstNumber} / ${secondNumber} = ${firstNumber/secondNumber}`)
-    }else{
-        console.log("Invalid operation");
-        
+const createUser = (): User[] => {
+  let users: User[] = [];
+  for (let i = 0; i <= 10; i++) { // Adjusted loop to run 11 times
+    let userObj: User = {
+      id: i,
+      pin: 1000 + i,
+      name: faker.person.fullName(),
+      accountNumber: Math.floor(100000000 * Math.random() * 90000000),
+      balance: 1000000 * i,
+    };
+    users.push(userObj);
+  }
+  return users;
+};
+
+// ATM machine
+const atmMachine = async (users: User[]) => {
+  try {
+    const response = await inquirer.prompt({
+      type: "number",
+      message: "Enter your PIN code",
+      name: "pin",
+    });
+
+    const findingUser = users.find((user) => user.pin === response.pin);
+
+    if (findingUser) {
+      console.log(chalk.green(`Welcome ${findingUser.name} to your Account`));
+      await atmFunc(findingUser);
+    } else {
+      console.log(chalk.red("Invalid User Pin"));
     }
-}
+  } catch (error) {
+    console.error(chalk.red("An error occurred: " + error));
+  }
+};
 
-main()
+const atmFunc = async (findingUser: User) => {
+  try {
+    const answer = await inquirer.prompt({
+      type: "list",
+      name: "Select",
+      message: "Choose the selection that you want to do",
+      choices: ["Withdraw", "Balance", "Deposit", "Exit"],
+    });
+
+    if (answer.Select === "Withdraw") {
+      const amount = await inquirer.prompt({
+        type: "number",
+        message: "Enter Amount",
+        name: "amount",
+      });
+      if (amount.amount > findingUser.balance) {
+        console.log(chalk.red("Insufficient Balance"));
+      } else {
+        findingUser.balance -= amount.amount;
+        console.log(chalk.green(`Withdraw amount: ${amount.amount}`));
+        console.log(chalk.yellow(`Total Balance: ${findingUser.balance}`));
+      }
+    }
+
+    if (answer.Select === "Balance") {
+      console.log(chalk.yellow(`Balance: ${findingUser.balance}`));
+    }
+
+    if (answer.Select === "Deposit") {
+      const deposit = await inquirer.prompt({
+        type: "number",
+        message: "Enter Deposit Amount",
+        name: "amount",
+      });
+      findingUser.balance += deposit.amount;
+      console.log(chalk.green(`Deposit amount: ${deposit.amount}`));
+      console.log(chalk.yellow(`Total Balance: ${findingUser.balance}`));
+    }
+
+    if (answer.Select === "Exit") {
+      console.log(chalk.blue("Thanks For Using This ATM...."));
+    }
+  } catch (error) {
+    console.error(chalk.red("An error occurred: " + error));
+  }
+};
+
+const users = createUser();
+atmMachine(users);
+
 
